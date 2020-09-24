57fabef0837dbc7f4b87de6639dfba60c9d9ca7f
userakai <65029150+userakai@users.noreply.github.com>
2020-09-24 20:58:28 +0200

    Update beam.cpp

--- beam.cpp
+++ beam.cpp
@@ -6,19 +6,19 @@ double vadd(double v1,double v2,double B1,double B2) {
   return v;
 }
 void generate(unsigned short int n,int N, double *p0, int *eff, bool *pass, double *BP) {
-  //definisco le componenti del campo magnetico
+  //Defining the magnetic field's components
   double Bx = 0, By = 0.3, Bz = 0;
   
-  //genero un fascio di particelle con le seguenti proprietà
+  //Particle beam properties
   double modv,phi,costh,vx,vy,vz,x,y,z;
   
-  //associo ad ogni particella gli estremi della traiettoria compiuta all'interno del cilindro
+  //Every particle will be associated to the extrems of their paths inside the detector
   double ax,ay,az,bx,by,bz;
 
   TRandom3* rg = new TRandom3(0);
   for(int i=0;i<N;i++) {
     
-    //il valore n determina l'utilizzo della funzione: se n=0 genera un flusso di particelle con momento iniziale uniformemente distribuito nell'intervallo [0.1,1000]; per altri valori di n genera particelle con momento iniziale determinato per 5 valori (0.1,1,10,100,1000)
+    //n will determine the distribution of initial momentum
     if(n==0)
       p0[i] = rg->Uniform(0.1,1000);
     else
@@ -33,14 +33,13 @@ void generate(unsigned short int n,int N, double *p0, int *eff, bool *pass, doub
     y = rg->Uniform(-3.9,3.9);
     z = 1.95;
     
-    //definisco delle variabili dove salvare le componenti di v che verranno utilizzate per aggiornarle tutte contemporaneamente
     double vx0, vy0, vz0;
     z=1.95;
     x=x+(vx*1.95)/vz;
     y=y+(vy*1.95)/vz;
     if(pow(x,2)+pow(y,2)<=0.25) {
       
-      //segno quali particelle entrano nel cilindro per sapere quali usare per calcolare il Bending Power
+      //Calculating the Bending Power for particles inside the detector
       pass[i]=1;      
       ax=x;
       ay=y;
@@ -55,7 +54,7 @@ void generate(unsigned short int n,int N, double *p0, int *eff, bool *pass, doub
 	 x = x+h*(vx+h/2*vx);
 	 y = y+h*(vy+h/2*vy);
 	 z = z+h*(vz+h/2*vz);
-	 //le particelle che attraversano tutta la lunghezza del cilindro senza uscire dai bordi andranno a contribuire al valore dell'efficienza del rivelatore
+	 //The fraction of particles passing through the whole detector will define its efficiency
          if(z>=2.95) {
 	   int cy = (int) (p0[i]/100);
 	   eff[cy]++;
@@ -65,7 +64,7 @@ void generate(unsigned short int n,int N, double *p0, int *eff, bool *pass, doub
        bx=x;
        by=y;
        bz=z;
-       //calcolo il Bending Power a partire dalle componenti della variazione di momento
+       //Calculating the Bending Power using the components of the variation of the momentum
        double BPx = ((bz-az)*By)-((by-ay)*Bz);
        double BPy = ((bx-ax)*Bz)-((bz-az)*Bx);
        double BPz = ((by-ay)*Bx)-((bx-ax)*By);
