c47eb946ada74cda12ba324ea338f5ce01b6a8c5
luciomoriconi <lucio.moriconi@piratech.io>
2021-01-23 23:58:53 +0100

    indentation correctio + minor fix

--- beam.cpp
+++ beam.cpp
@@ -1,75 +1,78 @@
 #include "header.h"
 using namespace std;
 
-double vadd(double v1,double v2,double B1,double B2) {
-  double v = B2*v1-B1*v2;
+double vadd(double v1, double v2, double B1, double B2)
+{
+  double v = B2 * v1 - B1 * v2;
   return v;
 }
-void generate(unsigned short int n,int N, double *p0, int *eff, bool *pass, double *BP) {
+void generate(unsigned short int n, int N, double *p0, int *eff, bool *pass, double *BP)
+{
   //Defining the magnetic field's components
   double Bx = 0, By = 0.3, Bz = 0;
-  
-  //Particle beam properties
-  double modv,phi,costh,vx,vy,vz,x,y,z;
-  
-  //Every particle will be associated to the extrems of their paths inside the detector
-  double ax,ay,az,bx,by,bz;
 
-  TRandom3* rg = new TRandom3(0);
-  for(int i=0;i<N;i++) {
-    
+  //Particle beam properties
+  double modv, phi, costh, vx, vy, vz, x, y, z;
+
+  //Every particle will be associated to the extrems of their paths inside the detector
+  double ax, ay, az, bx, by, bz;
+
+  TRandom3 *rg = new TRandom3(0);
+  for (int i = 0; i < N; i++)
+  {
     //n will determine the distribution of initial momentum
-    if(n==0)
-      p0[i] = rg->Uniform(0.1,1000);
+    if (n == 0)
+      p0[i] = rg->Uniform(0.1, 1000);
     else
-      p0[i] = 0.01*(pow(10,n));
-    modv = p0[i]/(sqrt(1+p0[i]));
-    phi = rg->Uniform(0,6.283);
+      p0[i] = 0.01 * (pow(10, n));
+    modv = p0[i] / (sqrt(1 + p0[i]));
+    phi = rg->Uniform(0, 6.283);
     costh = rg->Rndm();
-    vx = modv*(sqrt(1-costh))*sin(phi);
-    vy = modv*(sqrt(1-costh))*cos(phi);
-    vz = modv*(sqrt(costh));
-    x = rg->Uniform(-3.9,3.9);
-    y = rg->Uniform(-3.9,3.9);
+    vx = modv * (sqrt(1 - costh)) * sin(phi);
+    vy = modv * (sqrt(1 - costh)) * cos(phi);
+    vz = modv * (sqrt(costh));
+    x = rg->Uniform(-3.9, 3.9);
+    y = rg->Uniform(-3.9, 3.9);
     z = 1.95;
-    
+
     double vx0, vy0, vz0;
-    z=1.95;
-    x=x+(vx*1.95)/vz;
-    y=y+(vy*1.95)/vz;
-    if(pow(x,2)+pow(y,2)<=0.25) {
-      
+    z = 1.95;
+    x = x + (vx * 1.95) / vz;
+    y = y + (vy * 1.95) / vz;
+    if (pow(x, 2) + pow(y, 2) <= 0.25)
+    {
       //Calculating the Bending Power for particles inside the detector
-      pass[i]=1;      
-      ax=x;
-      ay=y;
-      az=z;
-	while(pow(x,2)+pow(y,2)<=1) {
-	 vx0=vx;
-	 vy0=vy;
-	 vz0=vz;
-	 vx = vx0+h*vadd(vy0+(h/2)*vadd(vy0,vz0,By,Bz),vz0+h/2*vadd(vy0,vz0,By,Bz),By,Bz);
-	 vy = vy0+h*vadd(vz0+(h/2)*vadd(vz0,vx0,Bz,Bx),vx0+h/2*vadd(vy0,vz0,Bz,Bx),Bz,Bx);
-	 vz = vz0+h*vadd(vx0+(h/2)*vadd(vx0,vy0,Bx,By),vy0+h/2*vadd(vx0,vy0,Bx,By),Bx,By);
-	 x = x+h*(vx+h/2*vx);
-	 y = y+h*(vy+h/2*vy);
-	 z = z+h*(vz+h/2*vz);
-	 //The fraction of particles passing through the whole detector will define its efficiency
-         if(z>=2.95) {
-	   int cy = (int) (p0[i]/100);
-	   eff[cy]++;
-	   break;
-	 }
-       }
-       bx=x;
-       by=y;
-       bz=z;
-       //Calculating the Bending Power using the components of the variation of the momentum
-       double BPx = ((bz-az)*By)-((by-ay)*Bz);
-       double BPy = ((bx-ax)*Bz)-((bz-az)*Bx);
-       double BPz = ((by-ay)*Bx)-((bx-ax)*By);
-       BP[i] = sqrt(pow(BPx,2)+pow(BPy,2)+pow(BPz,2));
+      pass[i] = 1;
+      ax = x;
+      ay = y;
+      az = z;
+      while (pow(x, 2) + pow(y, 2) <= 1)
+      {
+        vx0 = vx;
+        vy0 = vy;
+        vz0 = vz;
+        vx = vx0 + h * vadd(vy0 + (h / 2) * vadd(vy0, vz0, By, Bz), vz0 + h / 2 * vadd(vy0, vz0, By, Bz), By, Bz);
+        vy = vy0 + h * vadd(vz0 + (h / 2) * vadd(vz0, vx0, Bz, Bx), vx0 + h / 2 * vadd(vy0, vz0, Bz, Bx), Bz, Bx);
+        vz = vz0 + h * vadd(vx0 + (h / 2) * vadd(vx0, vy0, Bx, By), vy0 + h / 2 * vadd(vx0, vy0, Bx, By), Bx, By);
+        x = x + h * (vx + h / 2 * vx);
+        y = y + h * (vy + h / 2 * vy);
+        z = z + h * (vz + h / 2 * vz);
+        //The fraction of particles passing through the whole detector will define its efficiency
+        if (z >= 2.95)
+        {
+          int cy = (int)(p0[i] / 100);
+          eff[cy]++;
+          break;
+        }
+      }
+      bx = x;
+      by = y;
+      bz = z;
+      //Calculating the Bending Power using the components of the variation of the momentum
+      double BPx = ((bz - az) * By) - ((by - ay) * Bz);
+      double BPy = ((bx - ax) * Bz) - ((bz - az) * Bx);
+      double BPz = ((by - ay) * Bx) - ((bx - ax) * By);
+      BP[i] = sqrt(pow(BPx, 2) + pow(BPy, 2) + pow(BPz, 2));
     }
   }
 }
-    
--- main.cpp
+++ main.cpp
@@ -1,68 +1,70 @@
 #include "header.h"
 using namespace std;
-int main(int argc, char **argvw) {
+int main(int argc, char **argvw)
+{
   TApplication theApp("App", &argc, argvw);
   int N = 10000;
   double p0[N];
   double BP[N];
-  int eff[10];
+  int eff[10] = {0};
   bool pass[N];
   unsigned short int n = 0;
-  for(int i=0;i<10;i++)
-    eff[i]=0;
 
   //Filling the Bending Power histogram
   int power = -3;
-  TH1* h1 = new TH1D("data", "Bending Power", 100, 0, 2);
+  TH1 *h1 = new TH1D("data", "Bending Power", 100, 0, 2);
   h1->GetXaxis()->SetTitle("BP");
-  for(int i=0;i<1000;i++) {
-    for(int j=0;j<N;j++)
-      pass[i]=0;
-    generate(n,N,p0,eff,pass,BP);
-    for(int k=0;k<N;k++)
-      if(pass[k])
-	h1->Fill(BP[k],pow(p0[k],power));
+  for (int i = 0; i < 1000; i++)
+  {
+    for (int j = 0; j < N; j++)
+      pass[i] = 0;
+    generate(n, N, p0, eff, pass, BP);
+    for (int k = 0; k < N; k++)
+      if (pass[k])
+        h1->Fill(BP[k], pow(p0[k], power));
   }
 
   //Graphing the efficiency of the detector
-  double p0x[10],p0y[10];
-  for(int i=0;i<10;i++) {
-    p0x[i]=(i+1)*100;
-    double cy = (double) (eff[i]);
-    p0y[i]=cy/1000000;
+  double p0x[10], p0y[10];
+  for (int i = 0; i < 10; i++)
+  {
+    p0x[i] = (i + 1) * 100;
+    double cy = (double)(eff[i]);
+    p0y[i] = cy / 1000000;
   }
-  p0x[0]=0.1;
-  TGraph *gr1 = new TGraph (10,p0x,p0y);
+  p0x[0] = 0.1;
+  TGraph *gr1 = new TGraph(10, p0x, p0y);
   gr1->SetTitle("Efficiency");
   gr1->GetXaxis()->SetTitle("p (GeV)");
   gr1->SetMarkerColor(4);
   gr1->SetMarkerStyle(7);
 
   //Graphing mean and SD
-  double mean[N],std[N];
-  TH1* ms = new TH1D("0", "mean", 1000, 0, 10);
-  for(int i=0;i<5;i++) {
+  double mean[N], std[N];
+  TH1 *ms = new TH1D("0", "mean", 1000, 0, 10);
+  for (int i = 0; i < 5; i++)
+  {
     n++;
-    generate(n,N,p0,eff,pass,BP);
-    for(int j=0;j<1000;j++)
-      for(int k=0;k<N;k++)
-	ms->Fill(BP[j]);
+    generate(n, N, p0, eff, pass, BP);
+    for (int j = 0; j < 1000; j++)
+      for (int k = 0; k < N; k++)
+        ms->Fill(BP[j]);
     mean[i] = ms->GetMean();
     std[i] = ms->GetStdDev();
     ms->Reset();
   }
-  double P0X[5]={0.1,1,10,100,1000};
-  TGraph *gr3 = new TGraph (5,P0X,mean);
+  double P0X[5] = {0.1, 1, 10, 100, 1000};
+  TGraph *gr3 = new TGraph(5, P0X, mean);
   gr3->SetTitle("Mean");
   gr3->GetXaxis()->SetTitle("p0 (GeV)");
   gr3->SetMarkerColor(4);
   gr3->SetMarkerStyle(7);
-  TGraph *gr4 = new TGraph (5,P0X,std);
+  TGraph *gr4 = new TGraph(5, P0X, std);
   gr4->SetTitle("Standard Deviation");
   gr4->GetXaxis()->SetTitle("p0 (GeV)");
   gr4->SetMarkerColor(4);
   gr4->SetMarkerStyle(7);
-  
+
   //Canvas set-up
   TCanvas *c = new TCanvas();
   TPad *pad1 = new TPad("pad1", "pad 1", 0, 0.5, 0.5, 1);
